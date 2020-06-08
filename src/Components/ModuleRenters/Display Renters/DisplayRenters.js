5c57f118339870dc9c3efc1e62a260e5f2489ed9
Derek Lamarr <derek@macbook-pro.lan>
2020-06-08 13:57:34 -0700

    overflow contain and table improvements implemented on renters

--- src/Components/ModuleRenters/Display Renters/DisplayRenters.js
+++ src/Components/ModuleRenters/Display Renters/DisplayRenters.js
@@ -3,7 +3,7 @@ import React from 'react'
 import axios from 'axios'
 import {connect} from 'react-redux' 
 import {getRenters} from '../../../redux/reducers/houses'
-import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, IconButton} from '@material-ui/core'
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, IconButton, Typography, Tooltip } from '@material-ui/core'
 import DeleteIcon from '@material-ui/icons/Delete'
 import EditIcon from '@material-ui/icons/Edit';
 import {confirmDelete, pleaseSignIn, success} from '../../Functions/Sweetalerts'
@@ -36,19 +36,30 @@ function DisplayRenters(props) {
     const mappedRenters = props.renters ? props.renters.map((renter) => {
         return (
           <TableRow key={`renter-display-${renter.renter_id}`} className={`global-${renter.user_id.toString()}`}>
-            <TableCell align='left'><Button 
+            {/* <TableCell align='left'><Button 
             onClick={() => {
               if (props.user.data) {
                 toggleEmail(renter.email)
               } else {pleaseSignIn.fire()}
             }}
-          startIcon={<MailIcon />} size='small' color='primary' className='renter-email-button'>Email</Button></TableCell>
-            <TableCell align='left'>{renter.name}</TableCell>
-            <TableCell align="right">{renter.email}</TableCell>
-            <TableCell align="right">{renter.phone}</TableCell>
-            <TableCell align="right">{renter.address}</TableCell>
-            <TableCell align="right"><IconButton onClick={() => props.toggleUpdating('updating',renter)} color='primary' className='edit-icon'><EditIcon /> </IconButton></TableCell>
-            <TableCell align="left"><IconButton onClick={() => {if (props.user.data) {
+          startIcon={<MailIcon />} size='small' color='primary' className='renter-email-button'>Email</Button></TableCell> */}
+            <TableCell align='left'><Tooltip title={renter.name}><Typography className='table-typography-name' noWrap={true}>{renter.name}</Typography></Tooltip></TableCell>
+            <TableCell align='center'>
+              <div className='table-email-container'>
+              <Button 
+              onClick={() => {
+                if (props.user.data) {
+                  toggleEmail(renter.email)
+                } else {pleaseSignIn.fire()}
+              }}
+              startIcon={<MailIcon />} size='small' color='primary' className='renter-email-button'></Button>
+              <Tooltip title={renter.email}><Typography className='table-typography-email' noWrap={true}>{renter.email}</Typography></Tooltip>
+              </div>
+            </TableCell>
+            <TableCell align='left'><Tooltip title={renter.phone}><Typography className='table-typography-phone' noWrap={true}>{renter.phone}</Typography></Tooltip></TableCell>
+            <TableCell align='left'><Tooltip title={renter.address}><Typography className='table-typography-name' noWrap={true}>{renter.address}</Typography></Tooltip></TableCell>
+            <TableCell align="right"><IconButton className='action-button' onClick={() => props.toggleUpdating('updating',renter)} color='primary'><EditIcon /> </IconButton></TableCell>
+            <TableCell align="left"><IconButton className='action-button' onClick={() => {if (props.user.data) {
                         confirmDelete.fire({text: 'Are you sure you want to delete this renter? This action is irreversible.'})
                         .then((result) => {if (result.value) {deleteRenter(renter.renter_id)}})
                         } else {pleaseSignIn.fire()}}} 
@@ -64,17 +75,31 @@ function DisplayRenters(props) {
       <Table className='display-tasks-table' aria-label="simple table" size='small'>
         <TableHead>
           <TableRow>
-            <TableCell><Button 
+            {/* <TableCell><Button 
             onClick={() => {
               if (props.user.data) {
                 toggleEmail(props.renters && props.renters.map(elem => elem.email))
               } else {pleaseSignIn.fire()}
             }}
-             startIcon={<MailIcon />} size='small' color='primary' className='renter-email-button'>Email All</Button></TableCell>
+             startIcon={<MailIcon />} size='small' color='primary' className='renter-email-button'>Email All</Button></TableCell> */}
             <TableCell>Name</TableCell>
-            <TableCell align="right">Email</TableCell>
-            <TableCell align="right">Phone</TableCell>
-            <TableCell align="right">House</TableCell>
+            <TableCell align="left">
+            <div className='header-email-container'>
+              <Tooltip title='Email all renters'>
+              <Button 
+            onClick={() => {
+              if (props.user.data) {
+                toggleEmail(props.renters && props.renters.map(elem => elem.email))
+              } else {pleaseSignIn.fire()}
+            }}
+             startIcon={<MailIcon />} size='small' color='primary' className='renter-email-button'></Button>
+             </Tooltip>
+             Email
+
+             </div>
+             </TableCell>
+            <TableCell align="left">Phone</TableCell>
+            <TableCell align="left">House</TableCell>
             <TableCell colSpan={2} align="center">Actions</TableCell>
           </TableRow>
         </TableHead>
